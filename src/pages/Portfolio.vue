b333d7bec8e41fdacc94c046231329657681ad45
Lucas Scazzariello <lucas.scazza@gmail.com>
2021-03-04 20:42:42 -0300

    f

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -3,6 +3,7 @@
     <Container tag="div" fullScreen class="portfolio-header">
     <Container tag="div">
       <div class="portfolio-reel">
+        <h2 class="reel">Mirá nuestro Reel</h2>
         <video ref="video" poster="/media/postermanifiesto.jpg" controls frameborder="0">
           <source src="/media/manifiesto.mp4" type="video/mp4">
         </video>
